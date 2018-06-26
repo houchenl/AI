9b3b9da55a89c3aa501396002b1b7c1ead1d117f
houchenl <houchen890213@gmail.com>
2018-06-26 23:40:18 +0800

    python +=操作符与=的不同

--- knowledge/python/learn/operator/operator.py
+++ knowledge/python/learn/operator/operator.py
@@ -36,3 +36,42 @@ print('f is ', f)
 str = 'H' * 10
 print(str)      # HHHHHHHHHH
 
+
+
+# a += b != a = a + b
+a = 10
+b = [20]
+
+def test(num, array):
+    num += num
+    print(num, id(num))    # 20 4305329120，  不可修改变量，新建空间存储结果，新建变量指向新空间
+    array += array
+    print(array, id(array))    # [20, 20] 4345972552， 可修改变量，在原变量空间对原变量修改
+
+test(a, b)
+
+print(a, id(a))    # 10 4305328800
+print(b, id(b))    # [20, 20] 4345972552
+
+# python变量指向的是引用，函数调用时传递的是引用。
+# +=操作符对引用指向的数据进行操作，如果是可变类型，直接在数据上进行操作
+# 如果是不可变类型，重新开辟空间存储得到的值，并新建一个变量指定新空间。原空间值和原变量指向不变
+
+a = 10
+b = [20]
+
+def test2(num, array):
+    num = num + num
+    print(num, id(num))    # 20 4305329120
+    array = array + array
+    print(array, id(array))    # [20, 20] 4345972552
+
+test2(a, b)
+
+print(a, id(a))    # 10 4305328800
+print(b, id(b))    # [20] 4345971144
+
+# a = a + b，=右边的结果会存在一个新空间，=左边的a为新建变量，指向新空间
+
+
+
